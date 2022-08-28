9bbc5ac0d7852e11f7749f68e924ba616b5f2f38
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-28 19:20:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+  function submitData(name, email){
+    
+    let configurationObject = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then((resp) => resp.json())
+    .then(obj => {
+        document.body.innerHTML = obj.id
+      
+    }).catch((error) => {
+        alert("something went wrong");
+        document.body.innerHTML = error.message
+    })
+}
+submitData("Steve","steve@steve.com");
